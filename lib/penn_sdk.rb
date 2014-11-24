af353d492709ef888ba9ce880d6f6fcebad30c4b
Adel Qalieh <aqalieh95@gmail.com>
2014-11-24 16:17:05 -0500

    Add base HTTP request code

--- lib/penn_sdk.rb
+++ lib/penn_sdk.rb
@@ -1,5 +1,19 @@
 require "penn_sdk/version"
+require "httparty"
 
 module PennSDK
-  # Your code goes here...
+  class Penn
+    include HTTParty
+    base_uri 'esb.isc-seo.upenn.edu/8091/open_data'
+
+    def initialize(username, password)
+      @username = username
+      @password = password
+      @headers = {
+        "Authorization-Bearer" => username,
+        "Authorization-Token"  => password,
+        "Content-Type"         => "application/json; charset=utf-8"
+      }
+    end
+  end
 end
--- lib/penn_sdk/version.rb
+++ lib/penn_sdk/version.rb
@@ -1,3 +1,3 @@
-module PennSdk
+module PennSDK
   VERSION = "0.0.1"
 end
