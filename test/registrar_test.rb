f81a7d2035e1e62289f1cbf663e933071a71037b
Joseph Gao <gaoj@seas.upenn.edu>
2015-11-21 16:30:26 -0500

    added some more tests for registrar

--- test/registrar_test.rb
+++ test/registrar_test.rb
@@ -13,4 +13,17 @@ class RegistrarTest < Minitest::Test
     result = @registrar.course "CIS", "110"
     assert_equal "110", result[0]["course_number"]
   end
+
+  def test_section
+    result = @registrar.section "CIS", "110", "001"
+    assert_equal "LEC", result[0]["activity"]
+    assert_equal "CIS", result[0]["course_department"]
+    assert_equal "CIS 110001", result[0]["section_id"]
+    puts result
+  end
+
+  def test_search_params
+    result = @registrar.searchParams
+    assert(result[0]["acceptable_search_url_parameters_map"] != nil)
+  end
 end
